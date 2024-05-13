d1eb372ed62edf54749698aac695ac8cfbdca234
Farhan Sadeek <farhansadeek019@gmail.com>
2024-05-13 14:25:00 +0000

    Analysis + Streamlit App

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from ucimlrepo import fetch_ucirepo 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+st.write('Hello My name is Farhan')
